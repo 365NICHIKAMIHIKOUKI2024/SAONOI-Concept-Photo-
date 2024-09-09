3617944b68d285d2df97dc57bc9e625621f2878e
365NICHIKAMIHIKOUKI2024 <saonoi.bnk48thfc@gmail.com>
2024-09-09 22:16:53 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function tweetMessage() {
+    const message = "Jew 365 Concept Photo \n#JewBNK48 #2024_365NichinoKamihikoukiTH ";
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
+    window.open(tweetUrl, '_blank');
+}  
