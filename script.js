ffbdd9bc9d29e5cd511fb0922377ffa241f4cacc
365NICHIKAMIHIKOUKI2024 <saonoi.bnk48thfc@gmail.com>
2024-09-10 00:27:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tweetMessage() {
-    const message = "Saonoi 365 Concept Photo \n#SaonoiBNK48 #2024_365NichinoKamihikoukiTH ";
+    const message = "SAONOI 365 Concept Photo \n#SaonoiBNK48 #2024_365NichinoKamihikoukiTH ";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }  
